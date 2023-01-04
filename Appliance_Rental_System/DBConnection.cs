860fee64733a0658161b9f1cf481b82f943da776
Noel Phiri <59763043+Metro13@users.noreply.github.com>
2023-01-05 00:08:15 +0200

    inital commit

---
+++ Appliance_Rental_System/DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace Appliance_Rental_System
+{
+    internal class DBConnection
+    {
+        private int numrows;
+
+        public string loadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+        public int QueryExecuter(string query, Dictionary<string, object> args)
+        {
+
+            // database connection using Connection string from AppConfig
+            using (var connection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    foreach (var pair in args)
+                    {
+                        command.Parameters.AddWithValue(pair.Key, pair.Value);
+                    }
+                    // execute the Query & Returning Number of Rows
+                    numrows = command.ExecuteNonQuery();
+                }
+                return numrows;
+            }
+        }
+    }
+}
